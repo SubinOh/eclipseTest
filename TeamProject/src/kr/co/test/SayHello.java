5107171694f705e0fa511eb2743b768bf44d9340
OSB <subin.b.oh@gmail.com>
2018-02-21 10:58:09 +0900

    local실습

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("원격저장소 테스트를 위한 실습입니다");
+		System.out.println("충돌테스트를 위한 로컬실습입니다");
 	}
 }
